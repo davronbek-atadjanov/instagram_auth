7a5f1f3880ecd68fa8dbc4e082f67f1543b56fdd
Davronbekatadjanov <atadjanovdavronbek863@gmail.com>
2024-06-22 22:52:18 +0500

    SignUpSerializers update 1) create() metodi oraqli userni create qilish 2) validate_email() oraqli userni emalini validatsiyadan o'tkazish 3) to_representation() metodi oraqli yuborilayotgan datani upadate qilish va unga token() qo'shib yuborish dataga

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,8 @@
-from shared.utility import check_email_or_other
+
+from shared.utility import check_email_or_other, send_email
 from .models import User
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 class SignUpSerializer(serializers.ModelSerializer):
     # Foydalanuvchi identifikatori uchun faqat o'qish mumkin bo'lgan UUID maydoni
     id = serializers.UUIDField(read_only=True)
@@ -22,6 +23,18 @@ class SignUpSerializer(serializers.ModelSerializer):
             'id',
             'auth_status'
         )
+
+    def create(self, validated_data):
+        # `super` yordamida ota klassning `create` metodini chaqirib, foydalanuvchini yaratadi.
+        user = super(SignUpSerializer, self).create(validated_data)
+        # Yangi yaratilgan foydalanuvchi uchun verifikatsiya kodini yaratadi.
+        code = user.create_verify_code()
+        # Verifikatsiya kodini foydalanuvchining elektron pochta manziliga yuboradi.
+        send_email(user.email, code)
+        # Foydalanuvchini saqlaydi.
+        user.save()
+        # Yaratilgan foydalanuvchini qaytaradi.
+        return user
 
     def validate(self, data):
         # Ota klassning validate metodini chaqirish
@@ -48,4 +61,30 @@ class SignUpSerializer(serializers.ModelSerializer):
                 "success": False,
                 "message": "Siz email yuborishingiz kerak"
             }
+        return data
+
+    def validate_email(self, value):
+        # Email manzilini kichik harflarga o'zgartiradi.
+        value = value.lower()
+        # Agar berilgan email manzili bo'sh bo'lmasa va User modelida mavjud bo'lsa:
+        if value and User.objects.filter(email=value).exists():
+            # Xato xabari uchun ma'lumotlar tayyorlanadi.
+            data = {
+                "success": False,
+                "message": "Bu email dan allaqachon fodalanilgan "
+                # O'zbek tilida "Bu email allaqachon foydalanilgan" deb yozilgan.
+            }
+            # ValidationError chiqariladi va ma'lumotlar unga qo'shiladi.
+            raise ValidationError(data)
+        # Agar email mavjud bo'lmasa, uni qaytaradi.
+        return value
+
+    def to_representation(self, instance):
+        # Super klassning to_representation metodini chaqirib, instance ma'lumotlarini olish
+        data = super(SignUpSerializer, self).to_representation(instance)
+        # instance.token() metodidan foydalanib, foydalanuvchi uchun yangilanish tokenlarini olish
+        token_data = instance.token()
+        # data o'zgaruvchisiga yangilanish tokenlarni qo'shib qo'yish
+        data.update(token_data)
+        # Tuzilgan ma'lumotlarni qaytarish
         return data
