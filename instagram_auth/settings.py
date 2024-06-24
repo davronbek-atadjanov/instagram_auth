2fba30154277d8d798fe71e4954498d15d56be63
Davronbekatadjanov <atadjanovdavronbek863@gmail.com>
2024-06-24 10:29:09 +0500

    Added Swagger and Redoc API documentation 1) Configured schema_view with drf_yasg to provide Swagger UI for API documentation. 2) Added endpoints for Swagger UI and Redoc documentation: /swagger/ for Swagger UI. /redoc/ for Redoc UI.

--- instagram_auth/settings.py
+++ instagram_auth/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
+
+
     #local app
     'users',
     'shared'
